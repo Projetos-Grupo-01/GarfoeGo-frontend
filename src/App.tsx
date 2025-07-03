244440548864399252f1b208df10a61480b83e0a
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-07-03 12:15:25 -0300

    :ok_hand: Ajustes de componentes e rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
-import ListarProdutosHome from "./components/produtos/listarprodutos/ListarProdutoHome";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Sobre from "./components/sobre/Sobre";
+import Introducao from "./components/introducao/Introducao";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <div className="flex flex-col min-h-[64vh] bg-gray-200 ">
+        <div className="flex flex-col min-h-[80vh] bg-[#fff4e6]">
           <div className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/sobre" element={<Introducao />} />
+              <Route path="/quemsomos" element={<Sobre />} />
             </Routes>
           </div>
         </div>
